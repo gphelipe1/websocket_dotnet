0043a32d6d56d520135aa788609a67ebb6ca07b0
gphelipe1 <gpcfreitas@gmail.com>
2021-10-14 15:47:37 -0300

    Sync

---
+++ Hubs/MessageHubs.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using System;
+
+namespace websocket_test
+{
+    public class MessageHub: Hub
+    {
+        public Task SendMessageToAll(string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToCaller(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToUser(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("ReceiveMessage",message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(ex);
+        }
+    }
+}
